0bd6d829a998f74405587f4f43fad622f9eda9e8
solehasayyed31-afk <solehasayyed31@eng.rizvi.edu.in>
2025-11-10 21:56:49 +0530

    Add string palindrome check program

---
+++ string_palindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, len, flag = 0;
+
+    printf("Enter a string: ");
+    gets(str);  // reads input string
+
+    len = strlen(str);  // find length of string
+
+    for(i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - i - 1]) {
+            flag = 1;   // not palindrome
+            break;
+        }
+    }
+
+    if(flag == 0)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
